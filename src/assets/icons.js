8ec45c1c2644605322175095278021da2c50caa3
Максим Мухомедьяров <madmaxim@gmail.com>
2026-02-23 15:06:51 +0300

    impl: add static icon

---
+++ src/assets/icons.js
@@ -0,0 +1,51 @@
+/**
+ * URL иконок SVG из assets (для использования в Sidebar, InputPanel, messageTemplates, MessageComponent).
+ * Webpack обрабатывает импорты как asset/resource и подставляет итоговый путь.
+ */
+import AudioUrl from './Audio.svg';
+import VideoUrl from './Video.svg';
+import FileUrl from './File.svg';
+import FavoriteUrl from './Favorite.svg';
+import ImgUrl from './Img.svg';
+import PinUrl from './Pin.svg';
+import PinnedUrl from './Pinned.svg';
+import LinkUrl from './Link.svg';
+import LockOffUrl from './LockOff.svg';
+import LockOnUrl from './LockOn.svg';
+import CloseUrl from './Close.svg';
+import DeleteUrl from './Delete.svg';
+import DownloadUrl from './Download.svg';
+import DownloadsUrl from './Downloads.svg';
+import StopUrl from './Stop.svg';
+import MessagesUrl from './Messages.svg';
+import GeoUrl from './Geo.svg';
+import ImportUrl from './Import.svg';
+import ExportUrl from './Export.svg';
+import ReminderUrl from './Reminder.svg';
+import SettingUrl from './Setting.svg';
+import SmileUrl from './Smile.svg';
+
+export {
+  AudioUrl,
+  VideoUrl,
+  FileUrl,
+  FavoriteUrl,
+  ImgUrl,
+  PinUrl,
+  PinnedUrl,
+  LinkUrl,
+  LockOffUrl,
+  LockOnUrl,
+  CloseUrl,
+  DeleteUrl,
+  DownloadUrl,
+  DownloadsUrl,
+  StopUrl,
+  MessagesUrl,
+  GeoUrl,
+  ImportUrl,
+  ExportUrl,
+  ReminderUrl,
+  SettingUrl,
+  SmileUrl,
+};
